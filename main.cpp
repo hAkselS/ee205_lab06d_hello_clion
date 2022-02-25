cad8de03f4427d0e92024c04f7bf82a82f0cc36a
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2022-02-24 16:08:48 -1000

    Updated the hello clion main with my name and a fun fact.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main() {
    std::cout << "Hello, University of Hawaii, College of Engineering!" << std::endl;
    std::cout << "Lab 06d - Hello CLion - EE 205 - Spr 2022" << std::endl;
    std::cout << std::endl;
-   std::cout << "My name is:  <<YOUR NAME HERE>>" << std::endl;
-   std::cout << "A fun fact about me is <<TELL ME HOW MUCH YOU LOVE CATS>>" << std::endl;
+   std::cout << "My name is: Aksel" << std::endl;
+   std::cout << "A fun fact about me is I was born in the year of the tiger, so yea... I'm a cat guy" << std::endl;
 
    return 0;
 }
